12d9056d9054086eff317a6b796f64e0d5d963bb
Polina Shchepochkina <polina007@inbox.ru>
2022-08-20 23:57:03 +0300

    третий этап проектной работы

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       {
         test: /\.css$/,
